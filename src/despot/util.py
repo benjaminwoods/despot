1c224520b52773a0b279a789fda507cc02ff48ac
Benjamin Woods <ben@bjqw.me>
2020-07-10 21:51:22 +0100

    Upgrades to util.walkmodule.
    walkmodule now can skip internal objects (_...)  and private objects (__...)

--- src/despot/util.py
+++ src/despot/util.py
@@ -34,38 +34,78 @@ def walkdir(root,suf):
             if p.suffix in suf:
                 yield p
 
-def walkmodule(module):
+def walkmodule(module, skip_internal=False, skip_private=True, find_attr=False):
     # Walk through package/module
     _cache = []
     
     for handle in dir(module):
-        #if handle[0] == '_':
-        #    continue
+        if skip_internal:
+            # Skip all handles that begin with _
+            if len(handle) >= 1:
+                if handle[:1] == '_':
+                    continue
+        elif skip_private:
+            # Skip all handles that begin with __
+            if len(handle) >= 2:
+                if handle[:2] == '__':
+                    continue
         
         obj = getattr(module,handle)
         
-        # Test values?
+        # If not a module
         if not isinstance(obj, ModuleType):
+            # If the object is imported from another module,
+            # skip it if it is not a Python inbuilt type
             if (hasattr(obj, '__module__')
                 and obj.__module__ != module.__name__):
                     continue
             
+            if find_attr:
+                # If the object is a class,
+                # look for attributes + descriptors
+                if isinstance(obj, type):
+                    for attr in dir(obj):
+                        if skip_internal:
+                            # Skip all attrs that begin with _
+                            if len(attr) >= 1:
+                                if attr[:1] == '_':
+                                    continue
+                        elif skip_private:
+                            # Skip all attrs that begin with _
+                            if len(attr) >= 2:
+                                if attr[:2] == '__':
+                                    continue
+                        
+                        name = '::'.join((module.__name__, handle, attr))
+                        yield Path(module.__file__), name
+            
+            # Craft name from module.__name__ and the handle
             name = '::'.join((module.__name__, handle))
+            
+            # If the name is not in the cache, skip
             if name in _cache:
                 continue
             
             _cache.append(name)
             yield Path(module.__file__), name 
         
+        # If the object is a mod itself
         if isinstance(obj, ModuleType):
-            if '.'.join(obj.__name__.split('.')[:-1]) == module.__name__: 
-                for p, name in walkmodule(obj):
+            modulename = '.'.join(obj.__name__.split('.')[:-1])
+            
+            # if it is a submodule of module,
+            if modulename == module.__name__: 
+                # walk through the module
+                for p, name in walkmodule(obj,
+                                          skip_internal=skip_internal,
+                                          skip_private=skip_private,
+                                          find_attr=find_attr):
+                    # If the name is not in the cache, skip
                     if name in _cache:
                         continue
                     
                     _cache.append(name)
                     yield p, name
-        # !!! Check methods of classes
 
 class Singleton(type):
     _instances = {}
